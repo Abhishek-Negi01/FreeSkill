247916d618801cb1bfcf337097d10935fceec8ab
Abhishek Negi <negiabhishek75054@gmail.com>
2026-03-06 23:13:53 +0530

    feat(question-answer) : add question and answer controllers in backend with testing

---
+++ server/src/controllers/answer.controllers.js
@@ -0,0 +1,273 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Answer } from "../models/answer.models.js";
+import { Question } from "../models/question.models.js";
+
+const createAnswer = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { questionId, body } = req.body;
+
+  if (!questionId || !body || body.trim() === "") {
+    throw new ApiError(400, "question id and answer body are required");
+  }
+
+  // verify question exist
+  const question = await Question.findById(questionId);
+  if (!question) {
+    throw new ApiError(404, "question not found");
+  }
+
+  const answer = await Answer.create({
+    question: questionId,
+    answeredBy: userId,
+    body,
+  });
+
+  const populatedAnswer = await Answer.findById(answer._id)
+    .populate("answeredBy", "username fullname")
+    .populate("question", "title");
+
+  return res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        answer: populatedAnswer,
+      },
+      "answer created successfully",
+    ),
+  );
+});
+
+const getAnswersByQuestion = asyncHandler(async (req, res) => {
+  const { questionId } = req.params;
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  const answers = await Answer.find({ question: questionId })
+    .populate("answeredBy", "username fullname")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { answers }, "Answers fetched successfully"));
+});
+
+const updateAnswer = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { answerId } = req.params;
+
+  if (!answerId) {
+    throw new ApiError(401, "answer id is required");
+  }
+
+  const { body } = req.body;
+
+  if (!body || body.trim() === "") {
+    throw new ApiError(400, "Answer body is required");
+  }
+
+  const answer = await Answer.findById(answerId);
+
+  if (!answer) {
+    throw new ApiError(404, "Answer not found");
+  }
+
+  // only owner can edit the answer
+  if (answer.answeredBy.toString() !== userId.toString()) {
+    throw new ApiError(403, "user can only edit his or her answer");
+  }
+
+  const updatedAnswer = await Answer.findByIdAndUpdate(
+    answerId,
+    { $set: { body } },
+    { new: true },
+  ).populate("answeredBy", "username fullname");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { answer: updatedAnswer },
+        "Answer updated successfully",
+      ),
+    );
+});
+
+const deleteAnswer = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { answerId } = req.params;
+
+  if (!answerId) {
+    throw new ApiError(401, "answer id is required");
+  }
+
+  const answer = await Answer.findById(answerId);
+
+  if (!answer) {
+    throw new ApiError(404, "Answer not found");
+  }
+
+  // only owner can delete answer
+
+  if (answer.answeredBy.toString() !== userId.toString()) {
+    throw new ApiError(403, "user can only delete his or her own answers");
+  }
+
+  const deletedAnswer = await Answer.findByIdAndDelete(answerId);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { deletedAnswer }, "Answer deleted successfully"),
+    );
+});
+
+const acceptAnswer = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { answerId } = req.params;
+
+  if (!answerId) {
+    throw new ApiError(401, "answer id is required");
+  }
+
+  const answer = await Answer.findById(answerId).populate("question");
+
+  if (!answer) {
+    throw new ApiError(404, "Answer not found");
+  }
+
+  const question = answer.question;
+
+  // only question owner can accept answer
+  if (question.askedBy.toString() !== userId.toString()) {
+    throw new ApiError(403, "Only question owner can accept an answer");
+  }
+
+  question.acceptedAnswer = answerId;
+  await question.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { answer }, "Answer accepted successfully"));
+});
+
+const upvoteAnswer = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { answerId } = req.params;
+  if (!answerId) {
+    throw new ApiError(401, "answer id is required");
+  }
+
+  const answer = await Answer.findById(answerId);
+
+  if (!answer) {
+    throw new ApiError(404, "Answer not found");
+  }
+
+  const hasUpvoted = answer.upvotes.includes(userId);
+  const hasDownvoted = answer.downvotes.includes(userId);
+
+  if (hasUpvoted) {
+    // remove from upvotes
+    answer.upvotes = answer.upvotes.filter(
+      (id) => id.toString() !== userId.toString(),
+    );
+  } else {
+    // add in upvotes
+    answer.upvotes.push(userId);
+    // remove from downvotes
+    if (hasDownvoted) {
+      answer.downvotes = answer.downvotes.filter(
+        (id) => id.toString() !== userId.toString(),
+      );
+    }
+  }
+
+  await answer.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { answer }, "Vote updated successfully"));
+});
+
+const downvoteAnswer = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { answerId } = req.params;
+  if (!answerId) {
+    throw new ApiError(401, "answer id is required");
+  }
+
+  const answer = await Answer.findById(answerId);
+
+  if (!answer) {
+    throw new ApiError(404, "Answer not found");
+  }
+
+  const hasUpvoted = answer.upvotes.includes(userId);
+  const hasDownvoted = answer.downvotes.includes(userId);
+
+  if (hasDownvoted) {
+    // remove from downvotes
+    answer.downvotes = answer.downvotes.filter(
+      (id) => id.toString() !== userId.toString(),
+    );
+  } else {
+    // push in downvotes
+    answer.downvotes.push(userId);
+
+    // if it is present in upvotes then remove from there
+    if (hasUpvoted) {
+      answer.upvotes = answer.upvotes.filter(
+        (id) => id.toString() !== userId.toString(),
+      );
+    }
+  }
+
+  await answer.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { answer }, "Vote updated successfully"));
+});
+
+export {
+  createAnswer,
+  getAnswersByQuestion,
+  updateAnswer,
+  deleteAnswer,
+  acceptAnswer,
+  upvoteAnswer,
+  downvoteAnswer,
+};
---
+++ server/src/controllers/question.controllers.js
@@ -0,0 +1,294 @@
+import { Question } from "../models/question.models.js";
+import { Video } from "../models/video.models.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const createQuestion = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id; // from auth middleware
+  const { videoId, title, body } = req.body;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  if ([title, body].some((field) => !field || field?.trim() === "")) {
+    throw new ApiError(400, "title and body are required.");
+  }
+
+  // Verify video exists if provided
+  if (videoId) {
+    const video = await Video.findById(videoId);
+    if (!video) {
+      throw new ApiError(404, "Video not found");
+    }
+  }
+
+  const question = await Question.create({
+    video: videoId,
+    askedBy: userId,
+    title,
+    body,
+  });
+
+  const populatedQuestion = await Question.findById(question._id)
+    .populate("askedBy", "username fullname")
+    .populate("video", "title");
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        201,
+        { question: populatedQuestion },
+        "Question created successfully",
+      ),
+    );
+});
+
+const getQuestionsByVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required");
+  }
+
+  const questions = await Question.find({ video: videoId })
+    .populate("askedBy", "username fullname")
+    .populate("acceptedAnswer")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { questions }, "Questions fetched successfully"),
+    );
+});
+
+const getQuestionById = asyncHandler(async (req, res) => {
+  const { questionId } = req.params;
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  const question = await Question.findById(questionId)
+    .populate("askedBy", "username fullname")
+    .populate("video", "title")
+    .populate("acceptedAnswer");
+
+  if (!question) {
+    throw new ApiError(404, "Question not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { question }, "Question fetched successfully"));
+});
+
+const updateQuestion = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+  const { questionId } = req.params;
+  const { title, body } = req.body;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  if (!title && !body) {
+    throw new ApiError(400, "At least one field (title or body) is required");
+  }
+
+  const question = await Question.findById(questionId);
+
+  if (!question) {
+    throw new ApiError(404, "Question not found");
+  }
+
+  if (question.askedBy.toString() !== userId.toString()) {
+    throw new ApiError(403, "user can only edit his or her own question");
+  }
+
+  const updatedQuestion = await Question.findByIdAndUpdate(
+    questionId,
+    {
+      $set: { title, body },
+    },
+    { new: true },
+  )
+    .populate("askedBy", "username fullname")
+    .populate("video", "title");
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        question: updatedQuestion,
+      },
+      "Question updated successfully",
+    ),
+  );
+});
+
+const deleteQuestion = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { questionId } = req.params;
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  const question = await Question.findById(questionId);
+
+  if (!question) {
+    throw new ApiError(404, "Question not found");
+  }
+
+  // only can delete own question
+  if (question.askedBy.toString() !== userId.toString()) {
+    throw new ApiError(403, "user can only delete his or her own questions");
+  }
+
+  const deletedQuestion = await Question.findByIdAndDelete(questionId);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        deletedQuestion,
+      },
+      "Question deleted successfully",
+    ),
+  );
+});
+
+const upvoteQuestion = asyncHandler(async (req, res) => {
+  const userId = req?.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { questionId } = req.params;
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  const question = await Question.findById(questionId);
+
+  if (!question) {
+    throw new ApiError(404, "Question not found");
+  }
+
+  // if already upvoted
+  const hasUpvoted = question.upvotes.includes(userId);
+  const hasDownvoted = question.downvotes.includes(userId);
+
+  if (hasUpvoted) {
+    // Remove upvote
+    question.upvotes = question.upvotes.filter(
+      (id) => id.toString() !== userId.toString(),
+    );
+  } else {
+    // Add upvote and remove downvote if exist
+    question.upvotes.push(userId);
+    if (hasDownvoted) {
+      question.downvotes = question.downvotes.filter(
+        (id) => id.toString() !== userId.toString(),
+      );
+    }
+  }
+
+  await question.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { question }, "Vote updated successfully"));
+});
+
+const downvoteQuestion = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const { questionId } = req.params;
+
+  if (!questionId) {
+    throw new ApiError(400, "Question ID is required");
+  }
+
+  const question = await Question.findById(questionId);
+
+  if (!question) {
+    throw new ApiError(404, "Question not found");
+  }
+
+  // check if already downvoted
+
+  const hasUpvoted = question.upvotes.includes(userId);
+  const hasDownvoted = question.downvotes.includes(userId);
+
+  if (hasDownvoted) {
+    // Remove downvote
+    question.downvotes = question.downvotes.filter(
+      (id) => id.toString() !== userId.toString(),
+    );
+  } else {
+    // Add downvote and remove upvote if exist
+    question.downvotes.push(userId);
+    if (hasUpvoted) {
+      question.upvotes = question.upvotes.filter(
+        (id) => id.toString() !== userId.toString(),
+      );
+    }
+  }
+
+  await question.save();
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        question,
+      },
+      "Vote updated successfully",
+    ),
+  );
+});
+
+const getAllQuestions = asyncHandler(async (req, res) => {
+  const questions = await Question.find()
+    .populate("askedBy", "username fullname")
+    .populate("video", "title")
+    .populate("acceptedAnswer")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { questions }, "All questions fetched successfully"),
+    );
+});
+
+export {
+  createQuestion,
+  getQuestionsByVideo,
+  getQuestionById,
+  updateQuestion,
+  deleteQuestion,
+  upvoteQuestion,
+  downvoteQuestion,
+  getAllQuestions,
+};
